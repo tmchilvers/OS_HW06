a50230057345e659427fc850df5dd147e8317f23
tmchilvers <chilvers@chapman.edu>
2020-04-24 17:16:14 -0700

    Finished. Must check with VM now.

--- vmmgr.c
+++ vmmgr.c
@@ -1,5 +1,6 @@
 /*
-* 
+*
+*
 *
 * - TLB:
 *   - https://www.geeksforgeeks.org/translation-lookaside-buffer-tlb-in-paging/
@@ -34,14 +35,24 @@
 #define SIZE 10  //  size for string buffer
 
 //  ============================================================================
+//  STRUCTS
+typedef struct tlb {
+  int ladd; //  logical address
+  int fn; //  frame number
+} tlb;
+
+//  ============================================================================
 //  GLOBAL VARIABLES
 char buffer[SIZE];  // buffer to read each line from the address file
 int backingStore; //  Backing Store File int
 signed char* backingStoreMap;  //  backing store mmap
 FILE* addressFile;  //  Address File pointer
 int pageTable[PAGE_TABLE_SIZE]; //  create a pagetable
+struct tlb* t;  //  tlb struct
+
 signed char mainMem[MEM_SIZE]; //  create physical memory
 int pageFaults = 0; //  keep track of how many page faults there are
+int tlbHits = 0;  //  keep track of how many tlb hits there are
 
 //  ============================================================================
 //  MAIN PROGRAM
@@ -81,6 +92,16 @@ int main(int argc, char const *argv[])
   for(i = 0; i < PAGE_TABLE_SIZE; i++)
     pageTable[i] = -1;
 
+  //  Allocate memory for tlb (size of tlb entries)
+  t = malloc(sizeof(tlb)*TLB_ENTRS);
+
+  //  Set all values of the tlb to -1;
+  for(i = 0; i < PAGE_TABLE_SIZE; i++)
+  {
+    t[i].ladd = -1;
+    t[i].fn = -1;
+  }
+
   //  Create a mapping between backstore file data and the virtual memory
   backingStoreMap = mmap(0, MEM_SIZE, PROT_READ, MAP_PRIVATE, backingStore, 0);
 
@@ -91,10 +112,13 @@ int main(int argc, char const *argv[])
   int logicalAdd; //  logical address
   signed char value;  //  value at main memory
   unsigned char free = 0; //  iterate through page frames in main mem
+  int tlbSize = 0;  //  keep track of tlb size
+  int index = 0;  // index for fifo
 
   //  sort through the address file
   while(fgets(buffer, SIZE, addressFile) != NULL)
   {
+    int cont = 0; // 1 if there is a tlb hit
     //  Grab page number and offset value
     logicalAdd = atoi(buffer); // ex: 16916 = 0000 0000 0000 0000 0100 0010 0001 0100
     offset = logicalAdd & 255;    // & 255 =  0000 0000 0000 0000 0000 0000 0001 0100
@@ -104,7 +128,21 @@ int main(int argc, char const *argv[])
     frame = pageTable[logicalAdd]; //  Grab value from page table at page
 
     //  --------------------------------------------------------------------------
-    //  If there's a miss
+    //  See if pagenumber is in TLB
+    for(i = 0; i < TLB_ENTRS; i++)
+    {
+      //  Check if TLB is contains the logical address
+      if (logicalAdd == t[i].ladd)
+      {
+        frame = t[i].fn;  //  set frame to tlb frame
+        tlbHits++;  //  iterate tlb hits
+        cont = 1; //  don't need to update tlb, so pass
+        break;
+      }
+    }
+
+    //  --------------------------------------------------------------------------
+    //  If there's a miss in TLB and also Page Table
     if(frame == -1)
     {
       pageFaults++; //  keep track of how many pageFaults there are
@@ -121,7 +159,39 @@ int main(int argc, char const *argv[])
 
     //  If no miss, set physical address to frame grabbed from page table
     else
+    {
       physicalAdd = frame;
+    }
+
+    //  --------------------------------------------------------------------------
+    //  need to update tlb
+    if(cont == 0)
+    {
+      //  if tlb has not reached its full size
+      if (tlbSize < TLB_ENTRS)
+      {
+        //  set values of tlb to current logical address and frame
+        t[index].ladd = logicalAdd;
+        t[index].fn = free - 1; //  free - 1 is the frame number before iterating it
+        index++;
+        tlbSize++;
+      }
+
+      //  when tlb has reached its full size
+      else
+      {
+        //  FIFO, pop the first inputted value and move each value forward
+        for(i = 0; i < TLB_ENTRS - 1; i++)
+        {
+          t[i].ladd = t[i+1].ladd;
+          t[i].fn = t[i+1].fn;
+        }
+
+        //  Add the most recent logical address and frame to end of the queue
+        t[TLB_ENTRS - 1].ladd = logicalAdd;
+        t[TLB_ENTRS - 1].fn = free-1;
+      }
+    }
 
     //  --------------------------------------------------------------------------
     //  grab the value just entered into the main memory
@@ -136,7 +206,12 @@ int main(int argc, char const *argv[])
   //  Print rate of page faults below
   float pageFaultRate = ((float)pageFaults / 1000) * 100;  // 1000 = num of values in address file
   printf("\nPage Faults: %d\n", pageFaults);
-  printf("Page-fault Rate: %.2f%%\n", pageFaultRate);
+  printf("Page Faults Rate: %.2f%%\n", pageFaultRate);
+
+  //  Print rate of tlb hits below
+  float tlbHitRate = ((float)tlbHits / 1000) * 100;  // 1000 = num of values in address file
+  printf("\nTLB Hits: %d\n", tlbHits);
+  printf("TLB Hit Rate: %.2f%%\n", tlbHitRate);
 
   return 0;
 }
